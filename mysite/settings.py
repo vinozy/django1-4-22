dab6019036f53400db67479a6b0c4d2d4bdc988a
Ziyang Zhang <zz2732@columbia.edu>
2020-05-10 03:24:16 +0000

    add hosts and adopt app to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&zbj0z^2y(14j4m3hi_0x+*wlhtd!(^ii1sy$j4ny4hu#vczkw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adopt',
 ]
 
 MIDDLEWARE = [
