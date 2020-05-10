dc2510c179c76f6dc262dfe2fae162431752e402
Ziyang Zhang <zz2732@columbia.edu>
2020-05-10 10:32:40 +0000

    Add bootstrap templates.

--- adopt/views.py
+++ adopt/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Pet
 
@@ -6,23 +7,14 @@ def index(requests):
     return HttpResponse('Hello, world!!')
 
 def list_pets(requests):
-    response = '<ul>'  # unordered list
-
-    pets = Pet.objects.all()
-
-    for pet in pets:
-        response += f'<li><a href="/adopt/{pet.id}">{pet.name}</a></li>'
     
-    response += '</ul>'
-
-    return HttpResponse(response)
-
+    pets = Pet.objects.all()
+    
+    return render(requests, 'adopt/list.html',{'pets':pets})
+    
 def get_pet(requests,pet_id):
     
     pet = Pet.objects.get(id=pet_id)
-
-    
-
 
     return HttpResponse(f'Hello, Ziyang!!! This is {pet.name}.')
 # Create your views here.
